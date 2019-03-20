b6cc2d44c607f6dfbb980c8ff6ac7cc2a6bcb4bd
Kevin Chen <kchen01@stuy.edu>
2019-03-20 19:28:11 -0400

    stuck

--- Merge.java
+++ Merge.java
@@ -1,6 +1,6 @@
 public class Merge{
   public static void mergesort(int[]data){
-
+    mergesort(data,0,data.length - 1);
   }
   public static void mergesort(int[] data,int lo,int hi) {
     if(lo >= hi) {
@@ -17,23 +17,42 @@ public class Merge{
 
   // merge 2 already sorted list
   public static int[] merge(int[] data,int lo,int median, int high) {
-    int temp = 0;
+    int locounter = 0;
     int counter = 0;
     int i = 0;
-    while(i < median - lo + 1) {
-      if( data[lo] <= data[counter + median]) {
-        data[i + counter + lo] = data[lo];
-        i += 1;
-      }
-      else {
-        data[i + counter + lo] = data[median + counter];
+    int[] temp = new int[high - lo + 1];
+    while(median + counter <= high && i < temp.length && lo + locounter < median) {
+      
+      if(data[lo + locounter] > data[median + counter]) {
+        temp[i] = data[median + counter];
         counter += 1;
       }
+      else {
+        temp[i] = data[lo +locounter];
+        locounter += 1;
+      }
+      i += 1;
     }
-    while(counter + median < high - median) {
-      data[i + counter + lo] = data[median + counter];
-      counter += 1;
+    if (i > median) {
+    while (i < temp.length) {
+      temp[i] = data[i + counter];
+      i += 1;
     }
+  }
+  print(temp);
+  for (int j = 0; j < temp.length; j++) {
+
+    data[lo + j] = temp[j];
+  }
   return data;
+}
+
+  public static String print(int nums[]) {
+    String output = "";
+    for (int i = 0; i < nums.length; i++) {
+        output += nums[i] + " ";
+      }
+    System.out.println(output);
+    return output;
   }
 }
